f3a3af12a0111bd584eb83bd76e9dbb7c5c931e4
Ganesh Kumar <mvganeshk15sde@gmail.com>
2021-05-13 20:15:11 +0530

    added routes

---
+++ routes/sessions.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+const sessions = require("../models/sessions");
+
+router.get("/:sessionId", async (req, res) => {
+	try {
+		const { sessionId } = req.params;
+		const session = await sessions.findOne({ sessionId: sessionId });
+		res.json(session);
+	} catch (error) {
+		res.status(500).json({ errorMessage: error });
+	}
+});
+
+router.post("/", async (req, res) => {
+	try {
+		const session = await sessions.create(req.body);
+		await session.save();
+		res.json(session.sessionId);
+	} catch (error) {
+		res.status(500).json({ errorMessage: error });
+	}
+});
+
+module.exports = router;
