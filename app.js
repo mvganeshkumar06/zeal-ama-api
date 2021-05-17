81b63f5aac33ee4d7d6a042c93298a35a93f3205
Ganesh Kumar <mvganeshk15sde@gmail.com>
2021-05-17 13:26:51 +0530

    refactor : updated socket

--- app.js
+++ app.js
@@ -22,15 +22,16 @@ app.use("/session", sessionRoutes);
 let SESSION_ID;
 
 io.on("connection", (socket) => {
-	// When host joins a session
+	// When host joins the session
 	socket.on(
 		"host-join-session",
 		async (sessionId, hostSocketId, hostName) => {
-			// Join that session
+			SESSION_ID = sessionId;
+
+			// Make the host join the session
 			socket.join(sessionId);
 
 			console.log(`Host ${hostName} joined the session`);
-
 			// Save host socket Id to the DB
 			try {
 				const session = await sessions.findOne({ id: sessionId });
@@ -39,38 +40,44 @@ io.on("connection", (socket) => {
 			} catch (error) {
 				console.log(error);
 			}
-
-			// Save session id (later try something to store globally ?)
-			SESSION_ID = sessionId;
 		}
 	);
 
-	// When host sends ice candidate
-	socket.on("host-ice-candidate", async (iceCandidate, sessionId) => {
-		console.log("Received ICE candidate from host");
-
-		// Update the host ice candidate in the DB
-		try {
-			const session = await sessions.findOne({ id: sessionId });
-			session.host.iceCandidate = iceCandidate;
-			await session.save();
-		} catch (error) {
-			console.log(error);
-		}
-	});
-
 	// When offer is made by the host
-	socket.on("offer", async (offer, sessionId) => {
+	socket.on("offer", async (offer, sessionId, userSocketId) => {
 		console.log("Received offer from host");
 		try {
-			// Save host offer to the DB
-			const session = await sessions.findOne({ id: sessionId });
-			session.host.offer = offer;
-			await session.save();
+			// // Save host offer to the DB
+			// const session = await sessions.findOne({ id: sessionId });
+			// session.host.offer = offer;
+			// await session.save();
+
+			io.to(userSocketId).emit("offer-from-host", offer);
 		} catch (error) {
 			console.log(error);
 		}
 	});
+
+	// When host sends ice candidate
+	socket.on(
+		"host-ice-candidate",
+		async (iceCandidate, sessionId, userSocketId) => {
+			console.log("Received ICE candidate from host");
+			// Update the host ice candidate in the DB
+			try {
+				// const session = await sessions.findOne({ id: sessionId });
+				// session.host.iceCandidate = iceCandidate;
+				// await session.save();
+
+				io.to(userSocketId).emit(
+					"ice-candidate-from-host",
+					iceCandidate
+				);
+			} catch (error) {
+				console.log(error);
+			}
+		}
+	);
 
 	// When user joins a session
 	socket.on("join-session", async (sessionId, userSocketId, userName) => {
@@ -78,51 +85,28 @@ io.on("connection", (socket) => {
 			// Add the user to the database if not existing
 			const session = await sessions.findOne({ id: sessionId });
 			const user = session.users.find(
-				(user) => user.socketId === userSocketId
+				(user) => user.userName === userName
 			);
+
 			if (!user) {
 				session.users.push({
 					socketId: userSocketId,
 					userName: userName,
 				});
 				await session.save();
-
-				// Add the user to that session
-				socket.join(sessionId);
-
-				console.log(`User ${userName} joined the session`);
-
-				// Emit to everyone in the session that a user has joined except for the user
-				socket.to(sessionId).emit("user-joined-session", session.users);
-
-				// Emit the host ice candidate to the new user
-				socket
-					.to(sessionId)
-					.emit("ice-candidate-from-host", session.host.iceCandidate);
-
-				// Emit the host offer to the new user
-				socket
-					.to(sessionId)
-					.emit("offer-from-host", session.host.offer);
 			}
-		} catch (error) {
-			console.log(error);
-		}
-	});
 
-	// When user sends ice candidate
-	socket.on("user-ice-candidate", async (iceCandidate, sessionId) => {
-		console.log("Received ICE candidate from user");
+			// Make the user join the session
+			socket.join(sessionId);
 
-		// Get host socket Id from DB
-		try {
-			const session = await sessions.findOne({ id: sessionId });
-			const hostSocketId = session.host.socketId;
+			console.log(`User ${userName} joined the session`);
 
-			// Send the user ICE candidate to the host
-			socket
-				.to(hostSocketId)
-				.emit("ice-candidate-from-user", iceCandidate);
+			// Emit to everyone in the session that a user has joined except for the user
+			socket.broadcast.emit(
+				"user-joined-session",
+				session.users,
+				userSocketId
+			);
 		} catch (error) {
 			console.log(error);
 		}
@@ -132,20 +116,67 @@ io.on("connection", (socket) => {
 	socket.on("answer", async (answer, sessionId) => {
 		console.log("Received answer from user");
 		try {
-			// Get the host socket id from DB
-			const session = await sessions.findOne({ id: sessionId });
-			const hostSocketId = session.host.socketId;
+			// // Get the host socket id from DB
+			// const session = await sessions.findOne({ id: sessionId });
+			// const hostSocketId = session.host.socketId;
 			// Send the answer to the host
-			socket.to(hostSocketId).emit("answer-from-user", answer);
+			// Here io.to(hostSocketId).emit() is not working
+			// A workaround is the broadcast but listen only by host
+			socket.broadcast.emit("answer-from-user", answer);
 		} catch (error) {
 			console.log(error);
 		}
 	});
 
-	// // Indicate that a user has left the session
-	// socket.on("disconnect", () => {
-	// 	socket.to(sessionId).emit("userLeftSession");
-	// });
+	// When user sends ice candidate
+	socket.on("user-ice-candidate", async (iceCandidate, sessionId) => {
+		console.log("Received ICE candidate from user");
+		try {
+			// // Get host socket Id from DB
+			// const session = await sessions.findOne({ id: sessionId });
+			// const hostSocketId = session.host.socketId;
+			// Send the user ICE candidate to the host
+			// Here io.to(hostSocketId).emit() is not working
+			// A workaround is the broadcast but listen only by host
+			socket.broadcast.emit("ice-candidate-from-user", iceCandidate);
+		} catch (error) {
+			console.log(error);
+		}
+	});
+
+	// Indicate that a user has left the session
+	socket.on("leave-session", async (userSocketId) => {
+		try {
+			// Remove the user from the database if existing
+			const session = await sessions.findOne({ id: SESSION_ID });
+			const user = session.users.find(
+				(user) => user.socketId === userSocketId
+			);
+			if (user) {
+				session.users = session.users.filter(
+					(user) => user.socketId !== userSocketId
+				);
+				await session.save();
+				// Indicate to everyone in the session that the user has left
+				socket.broadcast.emit("user-left-session", session.users);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	});
+
+	// When the session is ended
+	socket.on("end-session", async (sessionId) => {
+		try {
+			// Disconnect the users
+			socket.broadcast.emit("disconnect-user");
+
+			// Delete the session details from DB
+			await sessions.deleteOne({ id: sessionId });
+		} catch (error) {
+			console.log(error);
+		}
+	});
 });
 
 app.get("/", (req, res) => {
